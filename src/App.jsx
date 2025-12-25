825321b25c7bec6ae09020bd1b71f91417328525
Kursant Anarkulov <anarkursant08@gmail.com>
2025-12-25 09:17:51 +0600

    добавиль css для main component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ function App() {
   return (
     <div className='body'>
       <header>
-        <img src="./assets/logo/logo.svg" alt="logo" />
+        <img src="./src/assets/logo/logo.svg" alt="logo" />
 
-        <div>
+        <div className="header-nav">
           <a href="#">About us</a>
           <button>Let’s Talk</button>
         </div>
       </header>
 
       <main>
-
+      <div className="background"></div>
       </main>
       <footer></footer>
     </div>
