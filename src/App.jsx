4b7c187a01e02060b8bf9ef3c089952f7e539edd
Kursant Anarkulov <anarkursant08@gmail.com>
2025-12-25 14:59:23 +0600

    закончил footer

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,22 @@ function App() {
         </section>
       </main>
       <footer>
-        
+        <section className="footer">
+          <div>
+            <p>Copyright © 2020 Invoriem</p>
+          </div>
+
+          <div className="footer-icons">
+            <p>info@invoriem.com</p>
+
+            <section>
+              <img src="/src/assets/icons/instagram-1.svg" alt="instagram" />
+              <img src="/src/assets/icons/facebook-2.svg" alt="facebook" />
+              <img src="/src/assets/icons/twitter-3.svg" alt="twitter" />
+              <img src="/src/assets/icons/linkedIn-4.svg" alt="linkedIn" />
+            </section>
+          </div>
+        </section>
       </footer>
     </div>
   )
