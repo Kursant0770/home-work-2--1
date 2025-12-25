76b794f251981c82ca580c7044908b48f3cdb8e4
Kursant Anarkulov <anarkursant08@gmail.com>
2025-12-25 14:05:15 +0600

    закончил box-3

--- src/App.jsx
+++ src/App.jsx
@@ -45,10 +45,21 @@ function App() {
         </section>
 
         <section className="box-3">
+          <div className="box-text">
+            <h1>We are Invoriem</h1>
 
+            <p>We fulfill international property developments</p>
+          </div>
+
+          <div className="box-video">
+            <img src="./src/assets/images/box-3-video.jpg" alt="video" className="video"/>
+            <img src="./src/assets/icons/box-3-play-video.svg" alt="play" className="play"/>
+          </div>
         </section>
       </main>
-      <footer></footer>
+      <footer>
+        
+      </footer>
     </div>
   )
 }
