e406c73df5c79494174e7f468a1734c42e57cd55
Kursant Anarkulov <anarkursant08@gmail.com>
2025-12-25 19:46:24 +0600

    Обновиление

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainOne from './components/MainOne'
-import MainThree from './components/MaintHree'
+import MainThree from './components/MainThree'
 import MainTwo from './components/MainTwo'
 
 function App() {
