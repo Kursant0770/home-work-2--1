ef591db7ae09324f18d4b8ebd7dcc5926c264276
Kursant Anarkulov <anarkursant08@gmail.com>
2025-12-26 11:24:52 +0600

    MainThree поменял

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainOne from './components/MainOne'
-import MaintHree from './components/MaintHree'
+import MaintHree from './components/MainThree'
 import MainTwo from './components/MainTwo'
 
 function App() {
